04957c41813bf746b9562ea73e815be4e4a5f096
dimel035 <45945660+dimel035@users.noreply.github.com>
2024-06-19 19:33:41 +0300

    interactions and new animals

--- VirtualWorld/Assets/scripts/Interactor.cs
+++ VirtualWorld/Assets/scripts/Interactor.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class followNPC : MonoBehaviour
+public class Interactor : MonoBehaviour
 {
     [SerializeField] Camera cam;
     public Interactable focus;
